8684811262e81292605bf021014f2a8ed74a230a
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-12-20 09:00:26 +0100

    added files

--- express-demo/index.js
+++ express-demo/index.js
@@ -9,8 +9,10 @@ app.get('/api/courses', (request, response)=>{
     response.send([1, 2, 3, 4, 5, 6, 7])
 })
 
-app.get('/api/posts/:year/:month', (request, response)=>{
-    response.send(request.query)
+app.get('/api/courses/:id', (request, response)=>{
+    const course =course.find(c=>c.id===parseInt(request.params.id))
+    if (!course)response.status(404).send('course with the given id is not found')
+    response.send(course)
 })
 
 app.listen(5000, ()=>console.log('listening'))
