d355c030f42ec7fde38d22242b269b6432a2ee97
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-12-20 09:10:10 +0100

    added files

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,18 +1,26 @@
 const  express=require('express')
 const  app=express()
 
+const course=[
+    {id:1, name:'english'},
+    {id:2, name:'math'},
+    {id:3, name:'igbo'},
+    {id:4, name:'health'},
+
+]
+
 app.get('/', (request, response)=>{
     response.send('Hello!')
 })
 
 app.get('/api/courses', (request, response)=>{
-    response.send([1, 2, 3, 4, 5, 6, 7])
+    response.send(course)
 })
 
 app.get('/api/courses/:id', (request, response)=>{
-    const course =course.find(c=>c.id===parseInt(request.params.id))
-    if (!course)response.status(404).send('course with the given id is not found')
-    response.send(course)
+    const course1 =course.find(c=>c.id===parseInt(request.params.id))
+    if (!course1)response.status(404).send('course with the given id is not found')
+    response.send(course1)
 })
 
 app.listen(5000, ()=>console.log('listening'))
