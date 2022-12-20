dc6b5f219345bd74d5bb77c768e6440d53986292
Mary Jane Okoroafor <maryjaneokoroafor77@gmail.com>
2022-12-20 10:02:31 +0100

    added files

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,7 +1,8 @@
 const  express=require('express')
 const  app=express()
+app.use(express.json())
 
-const course=[
+const courses=[
     {id:1, name:'english'},
     {id:2, name:'math'},
     {id:3, name:'igbo'},
@@ -13,15 +14,36 @@ app.get('/', (request, response)=>{
     response.send('Hello!')
 })
 
-app.get('/api/courses', (request, response)=>{
-    response.send(course)
+app.get('/api/course1', (request, response)=>{
+    response.send(courses)
 })
 
 app.get('/api/courses/:id', (request, response)=>{
-    const course1 =course.find(c=>c.id===parseInt(request.params.id))
-    if (!course1)response.status(404).send('course with the given id is not found')
-    response.send(course1)
+    const foundCourse =courses.find(c=>c.id===parseInt(request.params.id))
+    if (!foundCourse)response.status(404).send('course with the given id is not found')
+    response.send(foundCourse)
 })
+
+app.post('api/courses', (request, response)=>{
+    const  schema={
+        name: Joi.string().min(3).required()
+    }
+    const result=Joi.validate(request.body, schema)
+    if (result.error){
+        response.status(400).send(result.error.details[0].message)
+        return;
+    }
+    const postedCourse={
+        id:courses.length+1,
+        name:request.body.name
+    }
+    courses.push(postedCourse)
+    response.send(postedCourse)
+    return
+})
+
+
+
 
 app.listen(5000, ()=>console.log('listening'))
 
